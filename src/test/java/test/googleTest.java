4e9d259a75c59074774af6e6b377e931fe2e3047
Julio Atz <jatz@gtc.com.gt>
2025-03-10 14:46:07 -0600

    Commit pruebas completas

--- src/test/java/test/googleTest.java
+++ src/test/java/test/googleTest.java
@@ -35,33 +35,107 @@ public class googleTest {
         searchBox.sendKeys("Documentación de selenium");
         File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(screenshot, new File("capturas/capturaGoogle.png"));
-        Thread.sleep((long) (Math.random() * 5000));
+        Thread.sleep(2000);
         Actions actionsAntes = new Actions(driver);
         searchBox.submit();
-        Thread.sleep((long) (Math.random() * 5000));
         Actions actions = new Actions(driver);
-        //WebElement selectDocSelenium = driver.findElement(By.className("LC20lb"));
-        //actions.moveToElement(selectDocSelenium).perform();
-        Thread.sleep((long) (Math.random() * 5000));
-        //selectDocSelenium.click();
     }
 
     @Test
     public void testSenium() throws InterruptedException, IOException {
         driver.get("https://www.selenium.dev/documentation/");
+        String tabOriginal = driver.getWindowHandle();
         File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(screenshot, new File("capturas/capturaSelenium.png"));
+        /// //////////////////////////////////////////////////////
         driver.findElement(By.linkText("Overview")).sendKeys(Keys.CONTROL, Keys.RETURN);
         for (String tabOver : driver.getWindowHandles()) {
             driver.switchTo().window(tabOver);
         }
+        Thread.sleep(1000);
         File screenshotOver = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(screenshot, new File("capturas/capturaSeleniumOverview.png"));
+        FileUtils.copyFile(screenshotOver, new File("capturas/capturaSeleniumOverview.png"));
         driver.close();
+        driver.switchTo().window(tabOriginal);
+        /// ////////////////////////////////////
         driver.findElement(By.id("m-documentationwebdriver")).sendKeys(Keys.CONTROL, Keys.RETURN);
         for (String tabWebDriver : driver.getWindowHandles()) {
             driver.switchTo().window(tabWebDriver);
         }
+        Thread.sleep(1000);
+        File screenshotWebDriver = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotWebDriver, new File("capturas/capturaSeleniumWebDriver.png"));
+        driver.close();
+        driver.switchTo().window(tabOriginal);
+        /// //////////////////////////////////////
+        driver.findElement(By.id("m-documentationselenium_manager")).sendKeys(Keys.CONTROL, Keys.RETURN);
+        for (String tabSeleniumManeger : driver.getWindowHandles()) {
+            driver.switchTo().window(tabSeleniumManeger);
+        }
+        Thread.sleep(1000);
+        File screenshotSeleniumManager = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotSeleniumManager, new File("capturas/capturaSeleniumManager.png"));
+        driver.close();
+        driver.switchTo().window(tabOriginal);
+        /// ////////////////////////////////////////
+        driver.findElement(By.linkText("Grid")).sendKeys(Keys.CONTROL, Keys.RETURN);
+        for (String tabSeleniumGrid : driver.getWindowHandles()) {
+            driver.switchTo().window(tabSeleniumGrid);
+        }
+        Thread.sleep(1000);
+        File screenshotGrid = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotGrid, new File("capturas/capturaSeleniumGrid.png"));
+        driver.close();
+        driver.switchTo().window(tabOriginal);
+        /// /////////////////////////////////
+        driver.findElement(By.id("m-documentationie_driver_server")).sendKeys(Keys.CONTROL, Keys.RETURN);
+        for (String tabSeleniumIE : driver.getWindowHandles()) {
+            driver.switchTo().window(tabSeleniumIE);
+        }
+        Thread.sleep(1000);
+        File screenshotIE = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotIE, new File("capturas/capturaSeleniumIE.png"));
+        driver.close();
+        driver.switchTo().window(tabOriginal);
+        /// ///////////////////////////////////////
+        driver.findElement(By.linkText("IDE")).sendKeys(Keys.CONTROL, Keys.RETURN);
+        for (String tabSeleniumIDE : driver.getWindowHandles()) {
+            driver.switchTo().window(tabSeleniumIDE);
+        }
+        Thread.sleep(1000);
+        File screenshotIDE = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotIDE, new File("capturas/capturaSeleniumIDE.png"));
+        driver.close();
+        driver.switchTo().window(tabOriginal);
+        /// //////////////////////////////////////////
+        driver.findElement(By.id("m-documentationtest_practices")).sendKeys(Keys.CONTROL, Keys.RETURN);
+        for (String tabSeleniumTestPractice : driver.getWindowHandles()) {
+            driver.switchTo().window(tabSeleniumTestPractice);
+        }
+        Thread.sleep(1000);
+        File screenshotTestPractice = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotTestPractice, new File("capturas/capturaSeleniumTestPractice.png"));
+        driver.close();
+        driver.switchTo().window(tabOriginal);
+        /// //////////////////////////////////////////
+        driver.findElement(By.linkText("Legacy")).sendKeys(Keys.CONTROL, Keys.RETURN);
+        for (String tabSeleniumIDE : driver.getWindowHandles()) {
+            driver.switchTo().window(tabSeleniumIDE);
+        }
+        Thread.sleep(1000);
+        File screenshotLegacy = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotLegacy, new File("capturas/capturaSeleniumLegacy.png"));
+        driver.close();
+        driver.switchTo().window(tabOriginal);
+        /// //////////////////////////////////////////
+        driver.findElement(By.id("m-documentationabout")).sendKeys(Keys.CONTROL, Keys.RETURN);
+        for (String tabSeleniumIDE : driver.getWindowHandles()) {
+            driver.switchTo().window(tabSeleniumIDE);
+        }
+        Thread.sleep(1000);
+        File screenshotAbout = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshotAbout, new File("capturas/capturaSeleniumAbout.png"));
+        driver.close();
     }
 
     @AfterMethod
